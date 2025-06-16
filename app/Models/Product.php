d404bb46ae41471340a50cfc670e5f718c7deb7a
fadrian06 <franyeradriansanchez@gmail.com>
2025-06-16 10:16:00 -0400

    Add product fields to migration and the model as properties

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property-read int $id
+ * @property-read string $name
+ * @property-read float $unit_price
+ */
 class Product extends Model
 {
     //
