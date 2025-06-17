4dbd6745f0cfcc8252bfdf14edc07bd891b4f36c
fadrian06 <franyeradriansanchez@gmail.com>
2025-06-16 20:19:42 -0400

    Remove welcome page

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,9 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-  return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-  return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('/', 'dashboard')
+  ->middleware(['auth', 'verified'])
+  ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
   Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
