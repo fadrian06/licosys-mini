8e551afcb5929d653fda49252f53284df8952146
fadrian06 <franyeradriansanchez@gmail.com>
2025-06-28 14:12:51 -0400

    Remove duplicate auth middleware use in product pages

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::middleware('auth')->group(function () {
   Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
   Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
   Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-  Route::resource('products', ProductController::class)->middleware(['auth']);
+  Route::resource('products', ProductController::class);
 
   Route::get('preferences/theme/{theme}', function (string $theme): void {
     $_SESSION['theme'] = $theme;
