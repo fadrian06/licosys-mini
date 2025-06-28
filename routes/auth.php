82f03e1df168e72865321cd6180e7b2cc74a1d8a
fadrian06 <franyeradriansanchez@gmail.com>
2025-06-28 14:11:37 -0400

    Remove unused imports in auth routes

--- routes/auth.php
+++ routes/auth.php
@@ -1,37 +1,23 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-  Route::get('register', [RegisteredUserController::class, 'create'])
-    ->name('register');
-
+  Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
   Route::post('register', [RegisteredUserController::class, 'store']);
-
-  Route::get('login', [AuthenticatedSessionController::class, 'create'])
-    ->name('login');
-
+  Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
   Route::post('login', [AuthenticatedSessionController::class, 'store']);
 });
 
 Route::middleware('auth')->group(function () {
-  Route::post(
-    'email/verification-notification',
-    [EmailVerificationNotificationController::class, 'store']
-  )
+  Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
     ->middleware('throttle:6,1')
     ->name('verification.send');
 
-  Route::put('password', [PasswordController::class, 'update'])
-    ->name('password.update');
-
-  Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->name('logout');
+  Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+  Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
