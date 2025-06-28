4cdb86ce30a5b4e381ec8724d516bdfd015a225f
fadrian06 <franyeradriansanchez@gmail.com>
2025-06-28 16:48:22 -0400

    Remove bootstrap.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,11 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./bootstrap";
-
 import Alpine from "alpinejs";
+import axios from "axios";
 import * as bootstrap from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-window.Alpine = Alpine;
-window.bootstrap = bootstrap;
+globalThis.axios = axios;
+globalThis.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+globalThis.Alpine = Alpine;
+globalThis.bootstrap = bootstrap;
 
 Alpine.start();
