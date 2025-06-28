0a1efa3670c0fe1297e7405c5e6e41cc6fea6647
fadrian06 <franyeradriansanchez@gmail.com>
2025-06-28 14:01:31 -0400

    Simplify route definitions

--- routes/web.php
+++ routes/web.php
@@ -6,26 +6,24 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'dashboard')->middleware(['auth'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+  Route::view('/', 'dashboard')->name('dashboard');
   Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
   Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
   Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+  Route::resource('products', ProductController::class)->middleware(['auth']);
 
-Route::resource('products', ProductController::class)->middleware(['auth']);
+  Route::get('preferences/theme/{theme}', function (string $theme): void {
+    $_SESSION['theme'] = $theme;
+  });
 
-Route::get('preferences/theme/{theme}', function (string $theme): void {
-  $_SESSION['theme'] = $theme;
-});
+  Route::get('preferences/taxes/bcv/{tax}', function (float $tax, Request $request): void {
+    $user = $request->user();
 
-Route::get('preferences/taxes/bcv/{tax}', function (float $tax, Request $request): void {
-  $user = $request->user();
-
-  if ($user instanceof User) {
-    $user->update(['bcv_tax' => $tax]);
-  }
+    if ($user instanceof User) {
+      $user->update(['bcv_tax' => $tax]);
+    }
+  });
 });
 
 require_once __DIR__ . '/auth.php';
